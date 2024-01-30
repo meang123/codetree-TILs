44cf04938a8a9c2ffe880de3be09c9fa58cb8a14
Meang Sung joo <70367965+meang123@users.noreply.github.com>
2024-01-31 05:09:32 +0900

    Title: 원형 순열에서의 인원 제거, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/원형 순열에서의 인원 제거/josephus-permutations.cpp
+++ 240131/원형 순열에서의 인원 제거/josephus-permutations.cpp
@@ -2,8 +2,55 @@
 using namespace std;
 
 //배열로 원형 큐 해결해보기 
-
 int main() 
+{
+    // 여기에 코드를 작성해주세요.
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    int n=0,k=0;
+    cin>>n>>k;
+
+    //arr%capacity
+    
+    
+    
+    queue<int> q;
+    vector<int> arr;
+    arr.reserve(5000);
+
+
+    for(int i=1;i<=n;i++)
+    {
+        arr.emplace_back(i);
+    }
+    k--;
+
+    int idx=0,temp=0;
+    while(true)
+    {
+        temp = (idx+k)%n;
+        q.push(arr[temp]);
+        
+        arr.erase(arr.begin()+temp);
+        idx = temp;
+        n--;
+        if(n==0)
+        {
+            break;
+        }
+
+    }
+
+
+    while (!q.empty()) {
+        cout << q.front() << " ";
+
+        q.pop();
+    }
+
+    return 0;
+}
+/*int main() 
 {
     // 여기에 코드를 작성해주세요.
     ios::sync_with_stdio(0);
@@ -59,4 +106,4 @@ int main()
     }
 
     return 0;
-}
+}*/
